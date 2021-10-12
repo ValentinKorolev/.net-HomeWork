48e43449f38d30f65f702dd4d04cad44e3ed5f0a
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2021-10-12 12:37:25 +0300

    addMessage

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -7,6 +7,8 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ok, i want pull this message ");
         }
     }
 }
