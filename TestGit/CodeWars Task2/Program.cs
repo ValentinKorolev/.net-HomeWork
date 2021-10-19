5071bcd55fe1dc6616a991a2d5c60b5e8755ac1c
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2021-10-19 17:55:54 +0300

    Add Task 2(An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
    "Dermatoglyphics" --> true
    "aba" --> false
    "moOse" --> false (ignore letter casing))

---
+++ TestGit/CodeWars Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodeWars_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsIsogramm("Babe"));
+        }
+        static bool IsIsogramm(string str)
+        {
+            bool isIsogramm = true;
+            string lower = str.ToLower();
+            char[] stringCharacters = lower.ToCharArray();
+
+            for(int i = 0; i<=stringCharacters.Length-1; i++)
+            {
+                for(int j = i+1; j <= stringCharacters.Length - 1; j++)
+                {
+                    if (stringCharacters[i] == stringCharacters[j])
+                    {
+                      isIsogramm = false;
+                    }
+                }
+
+            }
+            return isIsogramm;
+        }
+    }
+}
