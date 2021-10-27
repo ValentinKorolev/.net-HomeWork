ac52926d74b541cfeab02baf0efbc20265f33d15
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2021-10-27 23:16:49 +0300

    Add Task3

---
+++ TestGit/CodeWars Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Usually when you buy something, you're asked whether your credit card number, 
+              phone number or answer to your most secret question is still correct. 
+              However, since someone could look over your shoulder, you don't want that shown on your screen.Instead, we mask it.
+              Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+              Examples
+                Kata.Maskify("4556364607935616"); // should return "############5616"
+                Kata.Maskify("64607935616");      // should return "#######5616"
+                Kata.Maskify("1");                // should return "1"
+                Kata.Maskify("");                 // should return ""   
+            
+            */
+
+            string text = "4556364607935616";
+                        
+            Console.WriteLine(Mastify(text));            
+        }
+        public static string Mastify(string text)
+        {
+            string modifyText = null;
+            if (text.Length > 4)
+            {
+                for (int i = 0; i < text.Length-4; i++)
+                {
+                    modifyText += "#";
+                }
+                modifyText += text.Substring(text.Length - 4, 4);
+            }
+            else if ((text.Length <= 4) || (text == "")) 
+            {
+                modifyText = text;
+            }
+
+            return modifyText;
+        }
+    }
+}
